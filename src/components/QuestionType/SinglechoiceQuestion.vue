c1b9c2ca1ac1abf26694de1564bf9a13dc1103e3
unknown <quanghuy203ld@gmail.com>
2024-02-01 21:29:52 +0700

    Complete other input field for multi and single choice

--- src/components/QuestionType/SinglechoiceQuestion.vue
+++ src/components/QuestionType/SinglechoiceQuestion.vue
@@ -35,7 +35,7 @@
         <input
           type="text"
           v-model="otherAnswer"
-          class="ml-2 mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block w-[50%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="ml-2 mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 inline-block w-[50%] p- dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Enter other answer"
           @input="updateOtherAns"
         />
